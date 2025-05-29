19dd1eaec964995a05e209aa7a544d19ae97f02b
Sumera-Zego <sumera.krishnappa@zego.com>
2025-05-29 13:28:09 +0100

    Footer aliggment fix

--- src/features/ui/MainHead.jsx
+++ src/features/ui/MainHead.jsx
@@ -1,7 +1,7 @@
 export default function MainHead() {
   return (
     <div className="flex flex-col gap-1">
-      <h2 className="mb-1 text-xl font-extrabold text-gray-900 dark:text-white lg:text-5xl">
+      <h2 className="mb-1 text-xl font-extrabold text-gray-900 dark:text-white lg:text-5xl text-center lg:text-left">
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
           How Does AI Understand?
         </span>
